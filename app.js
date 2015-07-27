1a88dbd21befe41166d17751851ef32e9a8e0846
matvey <giara.1995@gmail.com>
2015-07-27 13:06:36 +0300

    Add project files

---
+++ app.js
@@ -0,0 +1,39 @@
+angular.module('todoapp', ['firebase'])
+
+function ToDoCtrl($scope, angularFire) {
+    var fireData = new Firebase('https://dazzling-heat-327.firebaseio.com/ ');
+    angularFire(fireData, $scope, 'todos')
+    $scope.todos = [];
+    //Add new todo
+    $scope.addTodo = function () {
+        var newTodo = {
+            done: false,
+            text: $scope.todoText
+        };
+        $scope.todos.push(newTodo);
+        $scope.todoText = "";
+    };
+    //delete todo
+    $scope.removeTodo = function (start) {
+            $scope.todos.splice(start, 1);
+        }
+        //move down
+    $scope.move = function (index, direction) {
+        if (direction === 'up') {
+            if (index === 0) {
+                return;
+            }
+            index = index - 1;
+
+        }
+        if (direction === 'down') {
+            if (index === $scope.todos.length - 1) {
+                return;
+            }
+        }
+
+        var todo = $scope.todos[index];
+        $scope.todos.splice(index + 2, 0, todo);
+        $scope.todos.splice(index, 1);
+    }
+}
